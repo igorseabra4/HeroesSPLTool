d68bc3b9048d529992adbd399956798dcb678e12
Igor Seabra <igorseabra@gmail.com>
2018-03-15 04:33:33 -0300

    Initial commit

---
+++ HeroesSPLTool/Module1.vb
@@ -0,0 +1,128 @@
+﻿Imports System.IO
+
+Module Module1
+
+    Public Structure Vertex
+        Dim X As Single
+        Dim Y As Single
+        Dim Z As Single
+    End Structure
+
+    Sub Main()
+        If Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator <> "." Then
+            Threading.Thread.CurrentThread.CurrentCulture = New Globalization.CultureInfo("en-US")
+        End If
+
+        Dim Arguments As String() = Environment.GetCommandLineArgs()
+
+        Console.WriteLine("Heroes Spline Tool by igorseabra4")
+
+        If Arguments.Count > 1 Then
+            For i = 1 To Arguments.Count - 1
+                If Arguments(i).Substring(Arguments(i).Length - 3, 3).ToLower = "obj".ToLower Then
+                    ConvertOBJToSPL(Arguments(i))
+                End If
+                If Arguments(i).Substring(Arguments(i).Length - 3, 3).ToLower = "spl".ToLower Then
+                    ConvertSPLToOBJ(Arguments(i))
+                End If
+            Next
+        Else
+            Dim FilesInFolder As String() = Directory.GetFiles(Directory.GetCurrentDirectory)
+
+            For Each i As String In FilesInFolder
+                If i.Substring(i.Length - 3, 3).ToLower = "obj".ToLower Then
+                    ConvertOBJToSPL(i)
+                End If
+                If i.Substring(i.Length - 3, 3).ToLower = "spl".ToLower Then
+                    ConvertSPLToOBJ(i)
+                End If
+            Next
+        End If
+        Console.ReadKey()
+    End Sub
+
+    Sub ConvertOBJToSPL(FileName As String)
+        Console.WriteLine("Reading " + FileName)
+        Dim StringStream As String() = File.ReadAllLines(FileName)
+
+        Dim VertexStream As New List(Of Vertex)
+
+        For Each k As String In StringStream
+            If k.Length > 2 Then
+                If k.Substring(0, 2) = "v " Then
+                    Dim TempVertex As New Vertex
+                    TempVertex.X = Convert.ToSingle(k.Split(" ")(k.Split.Count - 3))
+                    TempVertex.Y = Convert.ToSingle(k.Split(" ")(k.Split.Count - 2))
+                    TempVertex.Z = Convert.ToSingle(k.Split(" ")(k.Split.Count - 1))
+                    VertexStream.Add(TempVertex)
+                End If
+            End If
+        Next
+
+        Console.WriteLine("Creating " + Path.ChangeExtension(FileName, "spl"))
+
+        Dim SPLWriter As New BinaryWriter(New FileStream(Path.ChangeExtension(FileName, "spl"), FileMode.Create))
+
+        SPLWriter.Write({&HC, 0, 0, 0, 0, 0, 0, 0, &HFF, &HFF, 0, &H14})
+        SPLWriter.BaseStream.Position = &H2C
+        SPLWriter.Write(VertexStream.Count)
+        SPLWriter.Write({1, 0, 0, 0})
+
+        For Each i As Vertex In VertexStream
+            SPLWriter.Write(i.X)
+            SPLWriter.Write(i.Y)
+            SPLWriter.Write(i.Z)
+        Next
+
+        SPLWriter.BaseStream.Position = 4
+        SPLWriter.Write(Convert.ToUInt32(SPLWriter.BaseStream.Length - &HC))
+
+        Console.WriteLine("Success.")
+    End Sub
+
+    Sub ConvertSPLToOBJ(FileName As String)
+        Console.WriteLine("Reading " + FileName)
+
+        Dim VertexStream As New List(Of Vertex)
+
+        Dim SPLReader As New BinaryReader(New FileStream(FileName, FileMode.Open))
+
+        SPLReader.BaseStream.Position = &H2C
+        Dim AmountOfVertices As Integer = SPLReader.ReadUInt32
+
+        SPLReader.BaseStream.Position = &H34
+
+        For i = 1 To AmountOfVertices
+            Dim TempVertex As New Vertex
+            TempVertex.X = SPLReader.ReadSingle
+            TempVertex.Y = SPLReader.ReadSingle
+            TempVertex.Z = SPLReader.ReadSingle
+            VertexStream.Add(TempVertex)
+        Next
+
+        Console.WriteLine("Creating " + Path.ChangeExtension(FileName, "obj"))
+        Dim SplineOBJWriter As New StreamWriter(New FileStream(Path.ChangeExtension(FileName, "obj"), FileMode.Create))
+
+        SplineOBJWriter.WriteLine("#Spline exported by Heroes Spline Tool")
+        SplineOBJWriter.WriteLine()
+
+        For Each i As Vertex In VertexStream
+            SplineOBJWriter.WriteLine("v " + i.X.ToString + " " + i.Y.ToString + " " + i.Z.ToString)
+        Next
+
+        SplineOBJWriter.WriteLine()
+
+        Dim Everynumber As String = ""
+
+        For i = 1 To VertexStream.Count
+            Everynumber += " " + i.ToString
+        Next
+
+        SplineOBJWriter.WriteLine("g " + "spline_" + FileName.Split("\").Last.Substring(0, FileName.Split("\").Last.Count - 4))
+        SplineOBJWriter.WriteLine("l" + Everynumber.ToArray)
+
+        SplineOBJWriter.Close()
+
+        Console.WriteLine("Success.")
+    End Sub
+End Module
---
+++ HeroesSPLTool/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' As informações gerais sobre um assembly são controladas por
+' conjunto de atributos. Altere estes valores de atributo para modificar as informações
+' associada a um assembly.
+
+' Revise os valores dos atributos do assembly
+
+<Assembly: AssemblyTitle("HeroesSPLTool")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("HeroesSPLTool")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+<Assembly: Guid("95d0b949-44a9-42e7-8df6-60167c9b8b24")>
+
+' As informações da versão de um assembly consistem nos quatro valores a seguir:
+'
+'      Versão Principal
+'      Versão Secundária 
+'      Número da Versão
+'      Revisão
+'
+' É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+' utilizando o "*" como mostrado abaixo:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
